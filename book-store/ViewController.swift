839eedf30194d7b81b5e3927cfc6543c80dde91d
s1thu <s1thu.w1n.5601@gmail.com>
2024-07-01 22:50:23 +0630

    create: Project Setup

---
+++ book-store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  book-store
+//
+//  Created by Sithu Win on 01/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
